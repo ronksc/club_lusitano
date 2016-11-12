5ce1fd9109d2fa3f5ddc0f92c62e8e8e69a93f3f
ronksc <ksc.ron@gmail.com>
2016-11-13 01:15:57 +0800

    update Ron 20161113-0115

--- wp-content/themes/club_lusitano/templates/content-facilities.php
+++ wp-content/themes/club_lusitano/templates/content-facilities.php
@@ -4,30 +4,147 @@
 			<div id="facilities">
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_1.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							28/f<br />O RETIRO
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								28/f<br />O RETIRO
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_2.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							27/f<br />SALÃO NOBRE DE CAMOES 
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								27/f<br />SALÃO NOBRE DE CAMOES 
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_3.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							26/f<br />SALA LEAL SENADO
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								26/f<br />SALA LEAL SENADO
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_4.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							25/f<br />SALA PENHA
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								25/f<br />SALA PENHA
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_5.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							25/f<br />SALA PRAIA GRANDE
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								25/f<br />SALA PRAIA GRANDE
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_6.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							25/f<br />SALA DE LILAU
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								25/f<br />SALA DE LILAU
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_7.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							24/f<br />PISO COMENDADOR ARNALDO DE OLIVEIRA SALES
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								24/f<br />PISO COMENDADOR ARNALDO DE OLIVEIRA SALES
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_8.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							24/f<br />SALA LORCHA
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								24/f<br />SALA LORCHA
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="facilities_item">
 					<img src="<?=get_stylesheet_directory_uri()?>/assets/img/facilities/img_9.jpg" class="img-responsive" />
+					<div class="overlay">
+						<div class="facilities_name">
+							23/f<br />SALA DE BILHAR
+						</div>
+						<div class="facilities_detail">
+							<div class="facilities_name">
+								23/f<br />SALA DE BILHAR
+							</div>
+							<div class="detail_content">
+								The grand ballroom with open views across Central, Government House plus the Zoological & Botanical Gardens<a href="#">See all photos»</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
