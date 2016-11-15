2a2d0c4a5db328802bc3eb353917cb8c12c3b7a8
alvarpoon <michiel@sedgwick-richardson.com>
2016-11-15 23:27:35 +0800

    facility cpt

---
+++ wp-content/themes/club_lusitano/lib/post-types.php
@@ -0,0 +1,33 @@
+<?php
+
+// mainpaage banner
+add_action('init', 'facility_register');
+function facility_register() {
+  $labels = array(
+      'name' => _x('Facility', 'post type general name'),
+      'singular_name' => _x('Facility', 'post type singular name'),
+      'add_new' => _x('Add facility', 'rep'),
+      'add_new_item' => __('Add facility'),
+      'edit_item' => __('Edit facility'),
+      'new_item' => __('New facility'),
+      'view_item' => __('View facility'),
+      'search_items' => __('Search facility'),
+      'not_found' =>  __('Nothing found'),
+      'not_found_in_trash' => __('Nothing found in Trash'),
+      'parent_item_colon' => ''
+  );
+  $args = array(
+      'labels' => $labels,
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true,
+      'query_var' => true,
+      'rewrite' => true,
+      'capability_type' => 'post',
+      'hierarchical' => true,
+      'menu_position' => 20,
+      'supports'      => array( 'title', 'editor'),
+  );
+  register_post_type( 'facility' , $args );
+}
+?>
